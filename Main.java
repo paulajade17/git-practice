9c89a741b471a0df3d91f9fa4a27fcc2c84f8693
Your Name <paulajade93@hotmail.co.uk>
2023-11-05 15:16:07 +0000

    edit main.java with println code.

--- Main.java
+++ Main.java
@@ -1,5 +1,40 @@
 class Main {
   public static void main(String[] args) {
+    System.out.println("I made changes to main java");
     System.out.println("Hello world! It's Sunday!");
+
+     System.out.println("I am going shoping to buy a lot of tasty food");
+
+        System.out.println("I am very tired and want to sleep");
+
+    int cookies = 20;
+
+    cookies = cookies -1;
+
+    System.out.println("I want to eat " + cookies + " cookies");
+
+    double bank_statement = 2000.00;
+    double bank_withdrawal = 1500.00;
+    double bank_deposit = 1000.00;
+    double bank_balance =(bank_statement - bank_withdrawal + bank_deposit);
+
+    System.out.println("My balance should be "  
+ + bank_balance);
+
+
+    
+
+    
+    
+
+    
+
+
+
+    
+     
+
+
+    
   }
 }
